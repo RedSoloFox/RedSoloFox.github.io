f14832ca7b1a070d8d28a3590f66165584ba62a7
redsolofox <RedSoloFox@gmail.com>
2017-05-15 10:32:22 -0400

    F

---
+++ debugging.py
@@ -0,0 +1,11 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at http://localhost:8000", PORT
+httpd.serve_forever()
